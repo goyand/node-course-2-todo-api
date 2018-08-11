7a4e0b31c9a0788aa9204e960f205bebb96b8e0a
後藤大輝 <daiki@D-10-19-180-71.dhcp4.washington.edu>
2018-08-11 16:39:22 -0700

    Add delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -22,17 +22,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Daiki',
-    age: 23,
-    location: 'Seattle'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert user', err);
-    }
-
-    console.log(result.ops[0]._id.getTimestamp());
-  })
+  // db.collection('Users').insertOne({
+  //   name: 'Daiki',
+  //   age: 23,
+  //   location: 'Seattle'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // })
 
 
   client.close();
