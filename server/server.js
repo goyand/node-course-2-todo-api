77481cf93c3e798eec7182639c9bcd13b903de94
後藤大輝 <daiki@D-10-19-180-71.dhcp4.washington.edu>
2018-08-12 14:51:59 -0700

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -43,13 +43,11 @@ app.get('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
-    
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
-  })
-
-
+  });
 });
 
 app.listen(3000, () => {
