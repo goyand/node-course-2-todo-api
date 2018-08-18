74f6e88d87c3e38c34856a634fc32a2a476c1867
後藤大輝 <daiki@gotoudaikinoMacBook-Air.local>
2018-08-17 21:55:32 -0700

    Add GET /users/me authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -97,7 +98,7 @@ app.patch('/todos/:id', (req, res) => {
   })
 })
 
-app.post('/user', (req, res) => {
+app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
@@ -110,6 +111,10 @@ app.post('/user', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+})
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
